775f972f0880325917207cd4621d2c123d900299
samhine <sam.hine27@gmail.com>
2019-09-23 18:42:54 +0100

    fixed reference issue

--- dawkins.py
+++ dawkins.py
@@ -1,4 +1,4 @@
-import random 
+import random, copy
 
 alph = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z', ' ']
 
@@ -13,13 +13,12 @@ def scorer(s, target):
 
 # Generate 28 random chars
 phr = [alph[random.randint(0,len(alph)-1)] for i in range(28)]
-print(phr)
+print(''.join(phr), 0)
 
 score = 0
-while score<10:
-    print(phr, score)
+while score<28:
     # Repeat this 100 times
-    phr_book = [phr for i in range(100)]
+    phr_book = [copy.copy(phr) for i in range(100)]
 
     # Apply mutation
     scores = []
@@ -35,5 +34,6 @@ while score<10:
     score = max(scores)
     phr_idx = scores.index(score)
     phr = phr_book[phr_idx]
+    print(''.join(phr), score)
 
 print(phr)
